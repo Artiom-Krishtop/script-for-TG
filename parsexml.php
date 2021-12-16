883da913170c9c416209608679f228727114d418
artiom <art_krishtop96@mail.ru>
2021-12-16 12:47:14 +0300

    ready parse.xml

--- parsexml.php
+++ parsexml.php
@@ -12,7 +12,7 @@ function dnd($text)
    echo '----------------------------------------------<br>';
 }
 
-$file_path = ['breakingmash.xml'];
+$file_path = ['breakingmash.xml', 'mash.xml'];
 
 class XMLParse
 {
@@ -30,8 +30,8 @@ class XMLParse
             $this->xml[$key] = simplexml_load_file($value);
 
             if ($this->xml[$key] === false) {
-               throw new Exception('File' . $value .  'not found');
                unset($this->xml[$key]);
+               throw new Exception("File $value not found");
             }
          }
 
@@ -60,15 +60,38 @@ class XMLParse
    
    public function parseXml()
    {
-      // $idLastItem = $this->getIdLastItem();
-      $arItems = $this->getItems();
-      dd($arItems);
       
-      foreach ($arItems as $item) {
-         if ($this->keyWordExists($item->description)) {
-            if (!$this->stopWordExists($item->description)) {
-               
-               $this->arItemLink[] = (string)$item->link;
+      list($arItems, $arChannel) = $this->getParams();
+      
+      foreach ($arItems as $key => $items) {
+
+         $channel = substr($arChannel[$key], strrpos($arChannel[$key], ':') + 1);
+
+         $channel = trim($channel);
+         
+         $idLastItem = $this->getIdLastItem($channel);
+
+         $counter = 0;
+
+         foreach ($items as $item) {
+
+            $counter++;
+
+            $itemid = substr($item->link, strrpos($item->link, '/') + 1);
+
+            if ($counter === 1) {
+               $this->setLastItemId($channel, $itemid);
+            }
+            
+            if ($itemid === $idLastItem) {
+               break;
+            }
+
+            if ($this->keyWordExists($item->description)) {
+               if (!$this->stopWordExists($item->description)) {
+                  
+                  $this->arItemLink[] = (string)$item->link;
+               }
             }
          }
       }
@@ -76,16 +99,19 @@ class XMLParse
       return $this->arItemLink;
    }
 
-   protected function getItems()
+   protected function getParams()
    {
       $arItems = [];
+      $arChannel = [];
 
       foreach ($this->xml as $key => $value) {
-         $arItems[] = $value->channel->item;
-         dnd($arItems);
+
+         $arItems[$key] = $value->channel->item;
+         $arChannel[$key] = $value->channel->title;
+
       }
 
-      return $arItems;
+      return [$arItems, $arChannel];
    }
    
    protected function keyWordExists(string $description)
@@ -114,19 +140,74 @@ class XMLParse
       return false;
    }
 
-   protected function getIdLastItem()
+   protected function getIdLastItem($nameChannel)
    {
       if(file_exists(self::LOG_LAST_ITEM_FILE))
       {
-         $file = file_get_contents(self::LOG_LAST_ITEM_FILE);
+         $text = file_get_contents(self::LOG_LAST_ITEM_FILE);
+         
+         $arChannel= explode(';', $text);    
+         
+         foreach ($arChannel as $value) {
+            
+            $value = explode('_', $value);
+            $chName  = array_shift($value);
+            if ($chName === $nameChannel) {
+               $lastId = array_shift($value);
 
-         $arrItems = explode(';', $file);    
-         dd($arrItems);     
+               return $lastId;
+            }
+         }
+      }
+
+      return null;
+   }
+
+   protected function setLastItemId($channel, $itemId)
+   {
+      $item = $channel . '_' . $itemId;
+
+      if (file_exists(self::LOG_LAST_ITEM_FILE)) {
+
+         $text = file_get_contents(self::LOG_LAST_ITEM_FILE);
+         
+         $arChannel = explode(';', $text); 
+         
+         $channel_exists = false;
+         
+         foreach ($arChannel as $key => $value) {
+            
+            $value = explode('_', $value);
+            $chName  = array_shift($value);
+
+            if ($chName === $channel) {
+
+               $arChannel[$key] = $item;
+
+               $text = implode(';', $arChannel);
+
+               file_put_contents(self::LOG_LAST_ITEM_FILE, $text);
+
+               $channel_exists = true;
+
+               break;
+            }
+         }
+         
+         if (!$channel_exists) {
+
+            $text .= ';' . $item;
+
+            file_put_contents(self::LOG_LAST_ITEM_FILE, $text);
+         }
+         
+      }else{
+         file_put_contents(self::LOG_LAST_ITEM_FILE, $item);
       }
    }
 }
 
-$a = new XMLParse($file_path, [ "Форбс", 'форбс','приёмная семья','-семья', "Сочи", '-5-летний']);
+$a = new XMLParse($file_path, [ "Форбс",'приёмная семья','-семья','бизнесмен', "Сочи", '5-летний']);
 
 $b = $a->parseXml();
 dnd($b);
